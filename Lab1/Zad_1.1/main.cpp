c976f1eb70395af5cd1e67e205731282ecdf856f
Caskyu <starkox1316@gmail.com>
2025-01-19 12:15:17 +0100

    2

--- Lab1/Zad_1.1/main.cpp
+++ Lab1/Zad_1.1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstdio>
 //Zad 1.1
 
 int main() {
-    std::cout << "Imię: Kacper Kamiński" << std::endl;
+    std::cout << "Imie Kacper Kaminski" << std::endl;
     std::cout << "Nr studenta: s32825" << std::endl;
-    std::cout << "Uczelnia: Polsko Japońska Akademia Technik komputerowych" << std::endl;
+    std::cout << "Uczelnia: Polsko Japonska Akademia Technik Kmputerowych" << std::endl;
 
     return 0;
 }
