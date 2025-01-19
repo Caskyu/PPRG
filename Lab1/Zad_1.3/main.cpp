4f764b5a892c3cde021feb0c2bad7dfafb9b2d54
Caskyu <starkox1316@gmail.com>
2025-01-19 12:15:27 +0100

    2

---
+++ Lab1/Zad_1.3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int liczba;
+
+    cout << "Podaj liczbe calkowita: ";
+    cin >> liczba;
+
+    if (liczba == 0) {
+        cout << "Liczba jest rowna 0." << endl;
+    } else if (liczba % 2 == 0) {
+        cout << "Liczba jest parzysta." << endl;
+    } else {
+        cout << "Liczba jest nieparzysta." << endl;
+    }
+
+    return 0;
+}
