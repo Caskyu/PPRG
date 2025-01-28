4dda80bede8dd8dd3a8b3d52951a332423fce578
Casky <starkox1316@gmail.com>
2025-01-28 06:53:10 +0100

    PPRG

--- Lab4/Zad_4.1/main.cpp
+++ Lab4/Zad_4.1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
@@ -13,21 +12,31 @@ int main() {
         return 1;
     }
 
-    vector<int> tablica(liczbaElementow);
+    int tablica[100];
     cout << "Podaj " << liczbaElementow << " elementow tablicy:" << endl;
 
     for (int i = 0; i < liczbaElementow; ++i) {
         cin >> tablica[i];
     }
 
-    int maksymalnyElement = tablica[0];
-    for (int i = 1; i < liczbaElementow; ++i) {
-        if (tablica[i] > maksymalnyElement) {
-            maksymalnyElement = tablica[i];
+    int maksWystapienia = 0;
+    int najczestszyElement = tablica[0];
+
+    for (int i = 0; i < liczbaElementow; ++i) {
+        int licznik = 0;
+        for (int j = 0; j < liczbaElementow; ++j) {
+            if (tablica[i] == tablica[j]) {
+                licznik++;
+            }
+        }
+        if (licznik > maksWystapienia) {
+            maksWystapienia = licznik;
+            najczestszyElement = tablica[i];
         }
     }
 
-    cout << "Najwiekszy element tablicy to: " << maksymalnyElement << endl;
+    cout << "Najczesciej wystepujacy element to: " << najczestszyElement
+         << " (wystapil " << maksWystapienia << " razy)" << endl;
 
     return 0;
 }
