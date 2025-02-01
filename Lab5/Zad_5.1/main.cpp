4f7ff4db8e26d473804211ca60ef9a4cfdcd531e
Casky <starkox1316@gmail.com>
2025-02-01 14:10:28 +0100

    PPRG

---
+++ Lab5/Zad_5.1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Podaj liczbę elementow wektora: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Liczba elementów musi być większa od zera." << endl;
+        return 1;
+    }
+
+    vector<int> wektor(n);
+    cout << "Podaj " << n << " elementow: ";
+    for (int i = 0; i < n; i++) {
+        cin >> wektor[i];
+    }
+
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (wektor[i] > wektor[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    cout << "Maksymalna wartosc: " << wektor[maxIndex] << endl;
+    cout << "Indeks maksymalnego elementu: " << maxIndex << endl;
+
+    return 0;
+}
